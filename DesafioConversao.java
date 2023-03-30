11c93ae330e2d742559087dfdf327219982ada5a
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-03-29 21:16:47 -0300

    Add files via upload

---
+++ DesafioConversao.java
@@ -0,0 +1,31 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("informe o primeiro salario: ");
+		String valor1 = entrada.next().replaceAll(",", ".");
+		
+		System.out.print("informe o segundo salario: ");
+		String valor2 = entrada.next().replaceAll(",", ".");
+		
+		
+		System.out.print("informe o terceiro salario: ");
+		String valor3 = entrada.next().replaceAll(",", ".");// replace substituir virgula por ponto
+		
+		double salario1 = Double.parseDouble(valor1);
+		double salario2 = Double.parseDouble(valor2);
+		double salario3 = Double.parseDouble(valor3);
+		
+		double media = (salario1 + salario2 + salario3) /3;
+		System.out.println("A média dos salários é: " + media);
+		
+		
+		entrada.close();
+	}
+}
